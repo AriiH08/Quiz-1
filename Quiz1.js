23404d9b7df2a5e4a375fccfcfcf945c10df908f
Ariana <ariana.hernandez.pena@est.una.ac.cr>
2025-02-22 22:59:55 -0600

    Commit inicial

---
+++ Quiz1.js
@@ -0,0 +1,62 @@
+// 1 ¿Cual es la salida del siguiente codigo?
+// // let x = 10;
+// (function() {
+//   console.log(x);
+//   let x = 20;
+// })();
+
+// d) ReferenceError
+
+//2 ¿Que se imprime en la consola?
+let h = 5;
+let v = h++;
+console.log(v);
+
+// b) 5
+
+//3 ¿Qué devuelve este código?
+console.log(typeof NaN);
+
+// a) numero
+
+// 4 ¿Cuál es la salida del siguiente código?
+let c = [1, 2, 3];
+let d = c;
+d.push(4);
+console.log(c);
+
+// b) [1, 2, 3, 4]
+
+//5 ¿Cuál es la salida de este código?
+let a = {};
+let b = a;
+console.log(a === b);
+
+// a) verdadero
+
+// 6 ¿Cuál es el resultado de la siguiente operación?
+console.log(1 + '1' - 1);
+
+// b) 10
+
+// 7 ¿Cuál es el resultado de esta expresión?
+console.log(0 == '0');
+
+// a) verdadero
+
+//8 ¿Qué imprime este código en la consola?
+console.log(typeof null);
+
+// b) object
+
+//9 ¿Cuál es la salida de este código?
+const arr = [1, 2, 3];
+const [x, y] = arr;
+console.log(y);
+
+// a) 2
+
+// 10 ¿Que retorna este codigo?
+console.log([] == false);
+
+// a) verdadero
